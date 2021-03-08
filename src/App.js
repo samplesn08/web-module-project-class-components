ccc053cac920fb21a4b353771e835fd14eb06a16
samplesn08 <samplesn08@gmail.com>
2021-03-08 16:07:21 -0500

    mvp working!  now to work on styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import './components/Todo.css';
+
+const todos = [
+  {
+    task: 'Organize Garage',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: Date.now(),
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -8,30 +22,50 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
-    this.state = [{
-      task: '',
-      id: Date.now(),
-      completed: false
-    }]
+    this.state = {
+      todos: todos
+    }
   }
-  changeHandler = event => {
+
+  toggleComplete = (clickedId) => {
     this.setState({
-      task: event.target.value
+      todos: this.state.todos.map((item) => {
+        if (item.id === clickedId) {
+          return {
+            ...item, completed: !item.completed,
+          };
+        } else {
+          return item;
+        }
+      }),
+    });
+  }
+
+  addTask = (taskName) => {
+    const newTask = {
+      name: taskName,
+      id: Date.now(),
+      completed: false,
+    }
+    this.setState({
+    todos: [...this.state.todos, newTask],
     })
   }
-  submitHandler = event => {
-    event.preventDefault();
-  }
-  clearHandler = event => {
 
-  }
+  clearHandler = event => {
+    event.preventDefault();
+    const newTodos = this.state.todos.filter(item => item.completed === false)
+    this.setState({
+      todos: newTodos
+    })
+}
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <TodoList changeHandler={this.changeHandler}/>
-        <TodoForm />
+        <TodoList todos={this.state.todos} toggleComplete={this.toggleComplete}/>
+        <TodoForm todos={this.state.todos} clearHandler={this.clearHandler} addTask={this.addTask} changeHandler={this.changeHandler}/>
       </div>
     );
   }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-const TodoForm = ({ changeHandler, submitHandler }) => {
-    return (
-        <form onSubmit={submitHandler}>
-            <input type='text' placeholder='New Task' onChange={changeHandler} />
-        </form>
-    )
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            itemText: ''
+        }
+    }
+    changeHandler = event => {
+        this.setState({
+          ...this.state, [event.target.name]: event.target.value
+        })
+      };
+
+    submitHandler = event => {
+        event.preventDefault();
+        this.props.addTask(this.state.itemText);
+        this.setState({
+          itemText: ''
+        })
+      };
+    
+    render() {
+        return (
+            <form onSubmit={this.submitHandler}>
+                <input type='text' name='itemText' value={this.state.itemText} placeholder='New Task' onChange={this.changeHandler} />
+                <button>Add Task</button><br></br><br></br>
+                <button onClick={this.props.clearHandler}>Clear Completed</button>
+            </form>
+        );
+    }
 }
 
 export default TodoForm;
